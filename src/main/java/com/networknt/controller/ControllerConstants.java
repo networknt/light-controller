307f048485d11694913068893f06f4dbb00be0b0
Steve Hu <stevehu@gmail.com>
2021-10-20 22:01:45 -0400

    fixes #91 get the health check topic from the configuration instead of constant (#92)

--- src/main/java/com/networknt/controller/ControllerConstants.java
+++ src/main/java/com/networknt/controller/ControllerConstants.java
@@ -6,6 +6,4 @@ public class ControllerConstants {
     public static String HOST = "lightapi.net";
     public static String CHECK = "check";
     public static int CHECK_FREQUENCY = 10; // check the health every 10 seconds by default.
-    public static String CHECK_TOPIC = "controller-health-check";
-
 }
